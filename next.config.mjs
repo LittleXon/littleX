e8c8192468da488355316f47768dcba7509eaeea
Aya EL OUAHDANI <ayael-ou@made-f0Br1s3.clusters.42paris.fr>
2025-03-24 17:14:03 +0100

    Fix: Enforce HTTPS redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        destination: 'https://littlexonsol.com/$1',
+        permanent: true,
+      },
+    ]
+  },
+}
+
+export default nextConfig
+
